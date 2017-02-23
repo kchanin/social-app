86b4435aed8a80688608c31a5e6732dc9acdadad
kchanin@bitbucket.org <kchanin@gmail.com>
2017-02-22 20:23:32 -0500

    Fancy UI cleanup

---
+++ kuti-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  kuti-social
+//
+//  Created by Ari Chanin on 2/22/17.
+//  Copyright © 2017 kuti. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY : CGFloat = 120.0/255.0
