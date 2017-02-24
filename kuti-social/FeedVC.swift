d4edb396f9ea0f0aa09f151f91c0583f9f92a581
kchanin@bitbucket.org <kchanin@gmail.com>
2017-02-24 11:56:06 -0500

    Image selection enabled

--- kuti-social/FeedVC.swift
+++ kuti-social/FeedVC.swift
@@ -10,16 +10,26 @@ import UIKit
 import SwiftKeychainWrapper
 import Firebase
 
-class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageAdd: CircleView!
     @IBOutlet weak var feedTblView: UITableView!
+    @IBOutlet weak var commentTxtFld: FancyField!
+    
     var posts = [Post]()
+    var imagePicker: UIImagePickerController!
+    var selectedImage: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         feedTblView.dataSource = self
         feedTblView.delegate = self
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        
         
         DataService.ds.REF_POSTS.observe(.value, with: {(snapshot) in
             if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot]{
@@ -34,8 +44,6 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             }
             self.feedTblView.reloadData()
         })
-
-
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -48,21 +56,45 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let post = self.posts[indexPath.row]
-        print("KUTI: \(post.caption)")
+        if let cell = feedTblView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as? PostCell{
+            cell.configureCell(post: posts[indexPath.row])
+            return cell
+        }else{
             
-        return feedTblView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath)
+            return PostCell()
+        }
     }
     
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage{
+            imageAdd.image = image
+            selectedImage = image
+        }else{
+            print("KUTI: A valid image was not selected")
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
 
     @IBAction func SignOutBtnPressed(_ sender: UITapGestureRecognizer) {
         if (KeychainWrapper.standard.removeObject(forKey: KEY_UID)) == true{
+            
+            DataService.ds.REF_POSTS.removeAllObservers()
+            
             print("KUTI: Key removed from keychain\n")
             try! FIRAuth.auth()?.signOut()
             print("KUTI: Signed out of out of Firebase\n")
 
+
             self.dismiss(animated: true, completion: nil)
         }
     }
+    
+    @IBAction func addPicTapped(_ sender: UITapGestureRecognizer) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func postImageBtnPressed(_ sender: Any) {
+    }
+    
+    
 }
